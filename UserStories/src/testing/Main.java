c56ec7ddb3e939c22be9a5c283c816b25a5290bd
YIZIBONNIE <yizinext@gmail.com>
2025-04-08 19:03:19 -0400

    update Main.java
    move addTask test to Main

--- UserStories/src/testing/Main.java
+++ UserStories/src/testing/Main.java
@@ -64,5 +64,29 @@ public class Main {
 			}
 		}
 	}
+	public void testTaskIsSuccessfullyAdded() {
+	 
+      Task task = new Task("1", "Laundry", "Wash clothes");
+      todo.addTask(task);
+      assertTrue(todo.getTasks().contains(task));
+  }
+
+  // Test that task count increases after adding
+  @Test
+  public void testTaskCountIncreasesAfterAdding() {
+      int before = todo.getTasks().size();
+      todo.addTask(new Task("2", "Study", "Read chapter 5"));
+      assertEquals(before + 1, todo.getTasks().size());
+  }
+
+  //Test that task has correct title and details
+  @Test
+  public void testTaskHasCorrectTitleAndDetails() {
+      Task task = new Task("3", "Exercise", "Run 3 miles");
+      todo.addTask(task);
+      Task added = todo.getTasks().get(0);
+      assertEquals("Exercise", added.getTitle());
+      assertEquals("Run 3 miles", added.getDetails());
+  }
 
 }
